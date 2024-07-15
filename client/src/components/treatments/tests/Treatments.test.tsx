c48818db503bc144ff6d899a27bf38804e4b54a7
Dongwook Kim <dongwook227@gmail.com>
2024-07-15 07:37:46 -0400

    query client and provider in tests

--- client/src/components/treatments/tests/Treatments.test.tsx
+++ client/src/components/treatments/tests/Treatments.test.tsx
@@ -1,7 +1,7 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen } from "@/test-utils";
 
 import { Treatments } from "../Treatments";
 
 test("renders response from query", () => {
-  // write test here
+  render(<Treatments />);
 });
