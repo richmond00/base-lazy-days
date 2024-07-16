ae2b77835cfb564f7d8cbbb253e36f096f5e9962
Dongwook Kim <dongwook227@gmail.com>
2024-07-16 06:44:03 -0400

    testing rendered query data

--- client/src/components/treatments/tests/Treatments.test.tsx
+++ client/src/components/treatments/tests/Treatments.test.tsx
@@ -2,6 +2,8 @@ import { render, screen } from "@/test-utils";
 
 import { Treatments } from "../Treatments";
 
-test("renders response from query", () => {
+test("renders response from query", async () => {
   render(<Treatments />);
+  const treatmenTitles = await screen.findAllByRole("heading", { name: /massage|facial|scrub/i })
+  expect(treatmenTitles).toHaveLength(3);
 });
